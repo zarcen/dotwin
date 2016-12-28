ba2661f237c6f6daac99cfd4e49312c075bf1f58
Wei-Chen Chen <weicche@microsoft.com>
2016-12-28 12:27:27 -0800

    Update init.cmd for private

--- init.cmd
+++ init.cmd
@@ -6,14 +6,28 @@
 :: "HKEY_CURRENT_USER\Software\Microsoft\Command Processor\AutoRun"
 @echo off
 
-:: Set dotwin Root Path
-if "%DOTWIN%"=="" (set DOTWIN=%USERPROFILE%\dotwin)
-set DOTWIN_PS1=%DOTWIN%\ps1
-set DOTWIN_BASHEE=%DOTWIN%\bashee
-
+:: Set Environment variables
+call :set_dotwin_var
+if errorlevel 1 exit /b
 :: Load personal alias definition
 call %DOTWIN%\alias.cmd
 
 :: Put bash wrapper scripts to be visible in PATH;
 :: Change order if prefer system default
-set PATH=%PATH%;%DOTWIN_BASHEE%
+path %PATH%;%DOTWIN_BASHEE%
+if exist %DOTWIN_PRIVATE% path %PATH%;%DOTWIN_PRIVATE%
+exit /b
+
+:: ******************************** SUBROUTINE ********************************
+:set_dotwin_var:
+    :: Set dotwin Root Path
+    if "%DOTWIN%"=="" (
+        set DOTWIN=%USERPROFILE%\dotwin
+    ) else (
+        exit /b 1
+    )
+    set DOTWIN_PS1=%DOTWIN%\ps1
+    set DOTWIN_BASHEE=%DOTWIN%\bashee
+    :: Private(or Corporate internal) stuff
+    if exist %DOTWIN%\private set DOTWIN_PRIVATE=%DOTWIN%\private
+exit /b
